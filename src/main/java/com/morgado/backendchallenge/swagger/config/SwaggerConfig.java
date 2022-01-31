7dd849cef9c15e8a43e4f922a08fd9af76a3f4c4
morgadope <pedron.morgdo@gmail.com>
2022-01-31 11:10:08 -0300

    Refactor

---
+++ src/main/java/com/morgado/backendchallenge/swagger/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.morgado.backendchallenge.swagger.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${spring.application.version}")
+    private String version;
+
+    @Value("${spring.application.description}")
+    private String description;
+
+    @Bean
+    public Docket apiV1(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName("V1")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.morgado.backendchallenge.api.v1"))
+                .paths(PathSelectors.any()).build()
+                .apiInfo(new ApiInfoBuilder()
+                        .version(version)
+                        .contact(contact())
+                        .title("API de validação de senhas")
+                        .description(description)
+                        .build());
+    }
+
+    private Contact contact(){
+        return new Contact(
+
+                "Pedro Morgado",
+                "https://github.com/morgadope",
+                "pedron.morgado@gmail.com");
+    }
+}
